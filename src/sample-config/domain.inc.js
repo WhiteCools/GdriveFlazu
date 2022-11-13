2616dceb7db0c4c123fb7df54d5545c63c8d6fc8
WhiteCools <73306092+WhiteCools@users.noreply.github.com>
2022-11-13 13:43:28 +0800

    Add files via upload

---
+++ src/sample-config/domain.inc.js
@@ -0,0 +1,11 @@
+module.exports = {
+	"id": 1, //number, unique for each domain
+	"name": "Flazu", //usually the name of parent institution/workspace account
+    "client_id": "219410691171-o9sipjjh7qlgtq8un9c58ifrmbbf0801.apps.googleusercontent.com",
+    "client_secret": "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",
+    "refresh_token": "1//04v_l8Yy96diMCgYIARAAGAQSNwF-L9Ir0HEdlim_WSJVndunNJsszJmnu_gAwSG5bGG8e45Q22-AREB_Oo3lgVrJk1GcLheF5h4",
+    "access_token": "", //can be empty, will be generated on the fly
+    "access_token_expires": "", //can be empty, will be generated on the fly
+    "disabled": false,
+    "errors": []
+}
